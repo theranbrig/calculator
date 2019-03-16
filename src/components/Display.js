5f89e1ccd24ff0a6449eca5f903c131933a7af7c
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-03-16 13:36:22 +0900

    updated with mobile responsiveness

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,8 +6,12 @@ const Display = () => {
   const { number, storedNumber, functionType } = useContext(NumberContext);
   return (
     <DisplayStyles>
-      <h2>{!number.length && !storedNumber ? '0' : number || storedNumber}</h2>
-      <p>{!storedNumber ? 'ENTER SOME NUMBERS' : `${storedNumber} ${functionType} ${number}`}</p>
+      <h2 className={storedNumber && storedNumber.length > 12 ? 'long-main-display' : undefined}>
+        {!number.length && !storedNumber ? '0' : number || storedNumber}
+      </h2>
+      <p className={storedNumber && storedNumber.length > 12 ? 'long-stored-display' : undefined}>
+        >{!storedNumber ? 'ENTER SOME NUMBERS' : `${storedNumber} ${functionType} ${number}`}
+      </p>
     </DisplayStyles>
   );
 };
--- src/components/styles/Styles.js
+++ src/components/styles/Styles.js
@@ -2,32 +2,45 @@ import styled from 'styled-components';
 
 export const CalculatorStyles = styled.div`
   background-color: #4abdac;
-  max-width: 100%;
+  width: 100%;
   min-height: 100vh;
   display: grid;
   justify-items: center;
-  grid-template-areas:
-    'display'
-    'numbers';
+  grid-template-rows: minmax(200px 350px) 1fr;
+  grid-template-columns: 1fr;
+  @media (max-width: 500px) {
+    max-width: 90%;
+    padding: 5%;
+  }
   .display {
     font-family: 'Orbitron', serif;
-    grid-area: display;
+    /* grid-area: display; */
+    margin: 0 !important;
+    width: 100%;
+    @media (max-width: 500px) {
+      width: 100%;
+      max-height: 200px;
+    }
     h1 {
       font-size: 4rem;
       color: white;
       text-align: center;
-      margin: 40px 0;
+      @media (max-width: 500px) {
+        font-size: 2rem;
+      }
     }
   }
   .number-pad {
-    grid-area: numbers;
+    /* grid-area: numbers; */
     display: grid;
     grid-gap: 10px;
     grid-template-columns: repeat(4, 1fr);
     padding: 0px 0px 30px;
     width: 450px;
-    .button-div:focus {
-      outline: none;
+    margin: 0 auto;
+    @media (max-width: 500px) {
+      width: 100%;
+      margin: 0;
     }
     button {
       width: 100%;
@@ -64,11 +77,15 @@ export const DisplayStyles = styled.div`
   grid-template-rows: 90px 50px;
   grid-template-columns: 1fr;
   border: 4px solid white;
-  margin: 10px;
-  width: 700px;
+  max-width: 700px;
+  margin: 10px auto;
   align-items: center;
   border-radius: 20px;
   background: #e17055;
+  @media (max-width: 500px) {
+    width: 95%;
+    grid-template-rows: 60px 40px;
+  }
   h2,
   p {
     text-align: center;
@@ -78,12 +95,24 @@ export const DisplayStyles = styled.div`
     font-size: 2.5rem;
     margin: 0;
     text-align: right;
-    padding: 0px;
     border-bottom: 4px solid white;
     padding: 15px 20px;
+    @media (max-width: 500px) {
+      font-size: 1.5rem;
+      padding: 10px;
+    }
+  }
+  h2.long-main-display {
+    font-size: 1.2rem;
   }
   p {
     margin: 5px 0;
     font-size: 1.3rem;
+    @media (max-width: 500px) {
+      font-size: 0.8rem;
+    }
+  }
+  p.long-stored-display {
+    font-size: 0.5rem;
   }
 `;
